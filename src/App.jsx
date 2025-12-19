22e4cd92e119a41fb6152a3953b6bae8cde36922
adrianodosanjoos <adriano.luizysa@gmail.com>
2025-12-19 17:12:29 -0300

    Atualização

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import './App.css'
 import Welcome from './components/Welcome'
 import Bomdia from './components/dia'
+import Pai from './components/Pai'
+import Descricao from './components/Descricao'
+import Cachorro from './components/Cachorro'
 
 function App() {
  
 
   return (
     <>
+    <Bomdia />
     <h1>Adriano</h1>
     <Welcome />
-     <Bomdia />
+     <Pai />
+     <Descricao nome = "Adriano" anos = {32}/>
+     <Cachorro nome = "Thor" raca = "PitBull"/>
     </>
     
   )
---
+++ src/components/Filho.jsx
@@ -0,0 +1,9 @@
+function Filho () {
+ return (
+    <div>
+        <p>Filho</p>
+    </div>
+ )
+}
+
+export default Filho;

57548fd688f82010470132b378acd61165830bbb
adrianodosanjoos <adriano.luizysa@gmail.com>
2026-01-08 16:54:23 -0300

    Atualização

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Bomdia from './components/dia'
 import Pai from './components/Pai'
 import Descricao from './components/Descricao'
 import Cachorro from './components/Cachorro'
+import Counter from './components/Counter'
 
 function App() {
  
@@ -16,6 +17,7 @@ function App() {
      <Pai />
      <Descricao nome = "Adriano" anos = {32}/>
      <Cachorro nome = "Thor" raca = "PitBull"/>
+     <Counter />
     </>
     
   )
---
+++ src/components/Counter.jsx
@@ -0,0 +1,9 @@
+import {useState} from 'react'
+
+const Counter = () => {
+     const [count, setCount] = useState(0);
+
+    return <div>{count}</div>;
+};
+
+export default Counter;
